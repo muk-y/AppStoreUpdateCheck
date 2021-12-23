be7ac36559e3d5778c9543185db2890d36c84d6a
ekmacmini43 <ekmacmini43@ekmacmini43s-Mac-mini.local>
2021-12-23 14:53:08 +0545

    Example project done

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -12,7 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkForUpdate { [weak self] updateTuple in
+            self?.showAlert(title: "Success", description: """
+                App version: \(updateTuple.appVersion ?? "N/A")\n
+                Update available: \(updateTuple.isUpdateAvailable)\n
+                Have to force update: \(updateTuple.haveToForceUpdate)\n
+                App store url: \(updateTuple.appStoreURL ?? "N/A")\n
+                """)
+        } failure: { [weak self] error in
+            self?.showAlert(title: "Error", description: error.localizedDescription)
+        }
+    }
+    
+    private func showAlert(title: String? = nil, description: String) {
+        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
     }
 
 
